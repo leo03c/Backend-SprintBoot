a3a7033c3fb9303fffbcf104bd891b87bb7334c0
Leonardo Castillo Díaz <leonardocastillod7@gmail.com>
2025-11-27 13:47:05 -0500

    Configurar Jackson para Lazy Loading

--- src/main/java/com/example/mi_backend/entity/IPV.java
+++ src/main/java/com/example/mi_backend/entity/IPV.java
@@ -22,7 +22,7 @@ public class IPV {
     private LocalDateTime fechaCreacion;
     
     
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "usuario_id", nullable = false)
     @JsonIgnoreProperties({"ipvs"}) // ← Evita mostrar los IPVs del usuario
     private Usuario usuario;
